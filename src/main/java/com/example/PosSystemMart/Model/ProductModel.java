c2850f5347addac3d142e30dd6f20e7c32fef01e
sophearith168-hengheng <himsophearith44@gmail.com>
2026-06-29 14:21:10 +0700

    create product model

--- src/main/java/com/example/PosSystemMart/Model/ProductModel.java
+++ src/main/java/com/example/PosSystemMart/Model/ProductModel.java
@@ -1,4 +1,24 @@
 package com.example.PosSystemMart.Model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "products")
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
 public class ProductModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String productName;
+    private String productDescription;
+    private Integer barcode;
+    private Double price;
+    private Integer quantity;
+
 }
