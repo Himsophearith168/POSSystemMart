e25cbc457fac993f74cdbcc87863ec8a742d93d1
sophearith168-hengheng <himsophearith44@gmail.com>
2026-06-29 13:25:28 +0700

    first commit

---
+++ src/main/java/com/example/PosSystemMart/PosSystemMartApplication.java
@@ -0,0 +1,13 @@
+package com.example.PosSystemMart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosSystemMartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosSystemMartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/PosSystemMart/PosSystemMartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PosSystemMart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosSystemMartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
